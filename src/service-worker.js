1a1c2c01a88c5ce2dcfe43c64e07404150bfd4ab
Arthur Stolyar <nekr.fabula@gmail.com>
2016-01-21 16:29:00 +0200

    Use child compiler for SW compilation

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,7 @@
-import SingleEntryDependency from 'webpack/lib/dependencies/SingleEntryDependency';
+import SingleEntryPlugin from 'webpack/lib/SingleEntryPlugin';
 import path from 'path';
 import webpack from 'webpack';
+import deepExtend from 'deep-extend';
 
 import getSource from './misc/get-source';
 import pathToBase from './misc/path-to-base';
@@ -19,16 +20,51 @@ export default class ServiceWorker {
   addEntry(plugin, compilation, compiler) {
     if (!this.entry) return Promise.resolve();
 
+    const name = plugin.entryPrefix + this.ENTRY_NAME;
+    const childCompiler = compilation.createChildCompiler(name, {
+      // filename: this.output
+      filename: name
+    });
+
     const data = JSON.stringify({
       data_var_name: this.SW_DATA_VAR
     });
     const loader = '!!' + path.join(__dirname, 'misc/sw-loader.js') + '?' + data;
-    const name = plugin.entryPrefix + this.ENTRY_NAME;
-    const dep = new SingleEntryDependency(loader + '!' + this.entry);
-    dep.loc = name;
+    const entry = loader + '!' + this.entry;
 
-    return new Promise((resolve) => {
-      compilation.addEntry(compiler.context, dep, name, () => {
+    childCompiler.context = compiler.context;
+    childCompiler.apply(new SingleEntryPlugin(compiler.context, entry, name));
+
+    (compiler.options.plugins || []).some((plugin) => {
+      if (plugin instanceof webpack.optimize.UglifyJsPlugin) {
+        const options = deepExtend({}, plugin.options);
+
+        options.test = new RegExp(name);
+        childCompiler.apply(new webpack.optimize.UglifyJsPlugin(options));
+
+        return true;
+      }
+    });
+
+    // Needed for HMR. offline-plugin doesn't support it,
+    // but added just in case to prevent other errors
+    childCompiler.plugin('compilation', function (compilation) {
+      if (compilation.cache) {
+        if (!compilation.cache[name]) {
+          compilation.cache[name] = {};
+        }
+
+        compilation.cache = compilation.cache[name];
+      }
+    });
+
+    return new Promise((resolve, reject) => {
+      childCompiler.runAsChild((err, entries, childCompilation) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
         resolve();
       });
     });
@@ -43,33 +79,18 @@ export default class ServiceWorker {
 
     if (this.entry) {
       const name = plugin.entryPrefix + this.ENTRY_NAME;
-      const entry = compilation.namedChunks[name];
+      const asset = compilation.assets[name];
 
-      if (!entry) {
+      if (!asset) {
         compilation.errors.push(
-          new Error('OfflinePlugin: ServiceWorker entry is not found in output chunks')
+          new Error('OfflinePlugin: ServiceWorker entry is not found in output assets')
         );
 
         return;
       }
 
-      if (entry.files.length > 1) {
-        compilation.warnings.push(
-          new Error('OfflinePlugin: ServiceWorker entry has more than one output file, only first will be used')
-        );
-      }
-
-      let entryAsset = entry.files[0];
-
-      entryAsset = compilation.assets[entryAsset];
-      entryAsset = entryAsset.source();
-
-      entry.files.forEach((file) => {
-        delete compilation.assets[file];
-      });
-
-      entry.files = [this.output];
-      source += '\n\n' + entryAsset;
+      delete compilation.assets[name];
+      source += '\n\n' + asset.source();
     }
 
     compilation.assets[this.output] = getSource(source);
