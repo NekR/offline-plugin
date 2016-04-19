42ad9fdc5d70f1c84bf4aa134fdc6d50cce28e87
Arthur Stolyar <nekr.fabula@gmail.com>
2016-04-19 16:38:41 +0300

    Implement changed strategy based on hashes + simply SW logic

--- src/misc/sw-template.js
+++ src/misc/sw-template.js
@@ -5,24 +5,21 @@ if (typeof DEBUG === 'undefined') {
 function WebpackServiceWorker(params) {
   const strategy = params.strategy;
   const assets = params.assets;
-  const hashesMap = params.hashesMaps;
+  let hashesMap = params.hashesMaps;
   const alwaysRevalidate = params.alwaysRevalidate;
   const ignoreSearch = params.ignoreSearch;
 
-  const STATIC_CACHE_TAG = 'static';
-  const TMP_CACHE_TAG = 'tmp';
-  const HASHES_TAG = 'hashes';
-
   const tagMap = {
     all: params.version,
-    changed: STATIC_CACHE_TAG
+    changed: params.version
   };
 
   const CACHE_PREFIX = params.name;
   const CACHE_TAG = tagMap[strategy];
   const CACHE_NAME = CACHE_PREFIX + ':' + CACHE_TAG;
-  const CACHE_TMP = CACHE_PREFIX + ':' + TMP_CACHE_TAG
-  const CACHE_HASHES = CACHE_PREFIX + ':' + HASHES_TAG;
+
+  const STORED_DATA_KEY = '__offline_webpack__data';
+  // new URL(, location).toString();
 
   mapAssets();
 
@@ -31,25 +28,30 @@ function WebpackServiceWorker(params) {
   self.addEventListener('install', (event) => {
     console.log('[SW]:', 'Install event');
 
-    const installing = strategy === 'changed' ?
-      cacheChanged('main', true) : cacheAssets('main');
+    let installing;
+
+    if (strategy === 'changed') {
+      installing = cacheChanged('main');
+    } else {
+      installing = cacheAssets('main');
+    }
 
     event.waitUntil(installing);
   });
 
-
   self.addEventListener('activate', (event) => {
     console.log('[SW]:', 'Activate event');
 
+    let activation = cacheAdditional();
+
+    /*if (strategy === 'changed') {
+      activation = activation.then(updateChanged);
+    }*/
+
     // Delete all assets which start with CACHE_PREFIX and
     // is not current cache (CACHE_NAME)
-    let activation = deleteObsolete();
-
-    if (strategy === 'changed') {
-      activation = activation.then(updateChanged);
-    }
-
-    activation = activation.then(cacheAdditional).then(() => {
+    activation = activation.then(deleteObsolete);
+    activation = activation.then(() => {
       if (self.clients && self.clients.claim) {
         return self.clients.claim();
       }
@@ -59,41 +61,33 @@ function WebpackServiceWorker(params) {
   });
 
   function cacheAdditional() {
-    if (!assets.additional.length) return;
+    if (!assets.additional.length) {
+      return Promise.resolve();
+    }
 
     if (DEBUG) {
       console.log('[SW]:', 'Caching additional');
     }
 
+    let operation;
+
     if (strategy === 'changed') {
-      return cacheChanged('additional')
+      operation = cacheChanged('additional')
     } else {
-      return cacheAssets('additional');
+      operation = cacheAssets('additional');
     }
+
+    // ignore fail of `additional` cache section
+    return operation.catch((e) => {
+      console.error('[SW]:', 'Cache section `additional` failed to load');
+    });
   }
 
-  function cacheAssets(section, options) {
-    const bustValue = params.version;
-    let batch;
-
-    if (strategy !== 'changed') {
-      batch = assets[section].map((asset) => {
-        return applyCacheBust(asset, bustValue);
-      });
-    } else if (alwaysRevalidate) {
-      batch = assets[section].map((asset) => {
-        if (alwaysRevalidate.indexOf(asset) !== -1) {
-          return applyCacheBust(asset, bustValue);
-        }
-
-        return asset;
-      });
-    } else {
-      batch = assets[section];
-    }
-
+  function cacheAssets(section) {
     return caches.open(CACHE_NAME).then((cache) => {
-      return addAllNormalized(cache, batch);
+      return addAllNormalized(assets[section], batch, {
+        bust: params.version
+      });
     }).then(() => {
       console.groupCollapsed('[SW]:', 'Cached assets: ' + section);
 
@@ -105,21 +99,21 @@ function WebpackServiceWorker(params) {
     });
   }
 
-  function cacheChanged(section, useTmpCache) {
-    let cache;
+  function cacheChanged(section) {
+    return getLastCache().then(args => {
+      if (!args) {
+        return cacheAssets(section);
+      }
 
-    const lastHashesMap = caches.match('/hashesMap', {
-      cacheName: CACHE_HASHES
-    }).catch(() => void 0);
+      const lastCache = args[0];
+      const lastKeys = args[1];
+      const lastMap = args[2];
 
-    return caches.open(CACHE_NAME).then((_cache) => {
-      cache = _cache;
-      return Promise.all([_cache.keys(), lastHashesMap]);
-    }).then(args => {
-      const keys = args[0];
-      const lastMap = args[1];
+      const lastHashedAssets = Object.keys(lastMap).map(hash => {
+        return lastMap[hash];
+      });
 
-      const paths = keys.map(req => {
+      const lastUrls = lastKeys.map(req => {
         const url = new URL(req.url);
         url.search = '';
 
@@ -127,64 +121,71 @@ function WebpackServiceWorker(params) {
       });
 
       const sectionAssets = assets[section];
+      const moved = [];
       const changed = sectionAssets.filter((url) => {
-        return paths.indexOf(url) === -1;
+        if (lastUrls.indexOf(url) === -1 || lastHashedAssets.indexOf(url) === -1) {
+          return true;
+        }
+
+        return false;
       });
 
-      if (lastMap) {
-        Object.keys(hashesMap).forEach((hash) => {
-          const asset = hashesMap[hash];
+      Object.keys(hashesMap).forEach(hash => {
+        const asset = hashesMap[hash];
 
-          if (lastMap.hasOwnProperty(hash)) {
-            if (lastMap[hash] !== asset) {
-              // moved
-              const changedIndex = changed.indexOf(asset);
+        if (
+          sectionAssets.indexOf(asset) === -1 ||
+          changed.indexOf(asset) !== -1 ||
+          moved.indexOf(asset) !== -1
+        ) return;
 
-              if (changedIndex !== -1) {
-                // moved and is in changed section,
-                // remove from there to prevent re-downloading of it
+        const lastAsset = lastMap[hash];
 
-                changed.splice(changedIndex, 1);
-              }
-            }
+        if (lastAsset && lastUrls.indexOf(lastAsset) !== -1) {
+          moved.push([
+            lastAsset,
+            asset
+          ]);
+        } else {
+          changed.push(asset);
+        }
+      });
 
-            return;
-          }
+      const movedResponses = Prommise.all(moved.map((pair) => {
+        return lastCache.match(pair[0]).then((response) => {
+          return [pair[1], response];
+        })
+      }));
 
-          if (sectionAssets.indexOf(asset) !== -1 && changed.indexOf(asset) === -1) {
-            changed.push(asset);
-          }
-        });
-      }
-
-      if (!changed.length) {
-        const targetCache = (useTmpCache ? caches.open(CACHE_TMP) : Promise.resolve(cache));
-
-        targetCache.then((_targetCache) => {
-          return addAllNormalized(_targetCache, changed)
-        }).then(() => {
-          console.groupCollapsed('[SW]:', 'Cached changed assets: ' + section);
-
-          changed.forEach((asset) => {
-            console.log('Asset:', asset);
+      return caches.open(CACHE_NAME).then((cache) => {
+        const move = movedResponses.then((responses) => {
+          return responses.map((pair) => {
+            return cache.put(pair[0], pair[1]);
           });
-
-          console.groupEnd();
         });
-      }
-    })
+
+        return Promise.all([
+          Promise.all(move),
+          addAllNormalized(cache, changed, {
+            bust: params.version
+          })
+        ]);
+      }).then(() => {
+        console.groupCollapsed('[SW]:', 'Cached changed assets: ' + section);
+
+        changed.forEach((asset) => {
+          console.log('Asset:', asset);
+        });
+
+        console.groupEnd();
+      });
+    });
   }
 
   function deleteObsolete() {
     return caches.keys().then((keys) => {
       const all = keys.map((key) => {
         if (key.indexOf(CACHE_PREFIX) !== 0 || key.indexOf(CACHE_NAME) === 0) return;
-
-        if (strategy === 'changed' && (
-          key.indexOf(CACHE_PREFIX + ':' + STATIC_CACHE_TAG) === 0 ||
-          key.indexOf(CACHE_TMP) === 0 ||
-          key.indexOf(CACHE_HASHES) === 0
-        )) return;
 
         console.log('[SW]:', 'Delete cache:', key);
         return caches.delete(key);
@@ -194,117 +195,30 @@ function WebpackServiceWorker(params) {
     });
   }
 
-  function updateChanged() {
-    let cache;
+  function getLastCache() {
+    return caches.keys().then(keys => {
+      let index = keys.length;
+      let key;
 
-    const lastHashesMap = caches.match('/hashesMap', {
-      cacheName: CACHE_HASHES
-    }).catch(() => void 0);
+      while (--index) {
+        key = keys[index];
 
-    return caches.open(CACHE_NAME).then((_cache) => {
-      cache = _cache;
-      return Promise.all([_cache.keys(), lastHashesMap]);
-    }).then((args) => {
-      const keys = args[0];
-      const lastMap = args[1];
-
-      const moved = [];
-      let deletion = keys.filter((req) => {
-        const url = new URL(req.url);
-        url.search = '';
-        const urlString = url.toString();
-
-        if (allAssets.indexOf(urlString) === -1) {
-          req._urlString = urlString;
-          return true;
+        if (key.indexOf(CACHE_PREFIX) === 0) {
+          break;
         }
+      }
+
+      if (!key) return;
+
+      return caches.open(key).then(cache => {
+        return cache.match(STORED_DATA_KEY);
+      }).then(response => {
+        if (!response) return;
+
+        return Promise.all([cache, cache.keys(), response.json()]);
       });
-
-      if (lastMap) {
-        Object.keys(lastMap).forEach((lastHash) => {
-          const lastAsset = lastMap[lastHash];
-
-          if (hashesMap.hasOwnProperty(lastHash)) {
-            const newAsset = hashesMap[lastHash];
-
-            if (lastAsset !== newAsset) {
-              // moved
-              moved.push([lastAsset, newAsset]);
-              // deletion is performed after movement is done
-              deletion.push(lastAsset);
-            }
-          } else {
-            // deleted
-            if (deletion.indexOf(lastAsset) === -1) {
-              deletion.push(lastAsset);
-            }
-          }
-        });
-      }
-
-      let operation = Promise.resolve();
-
-      if (moved.length) {
-        operation = operation.then(() => {
-          return moved.map((pair) => {
-            return cache.match(pair[0]).then((response) => {
-              if (response) {
-                return cache.put(pair[1], response);
-              }
-
-              return addAllNormalized(cache, [pair[1]]);
-            });
-          });
-        }).then((moved) => {
-          return Promise.all(moved);
-        }).then(() => {
-          console.groupCollapsed('[SW]:', 'Cached moved assets: ' + section);
-
-          moved.forEach((pair) => {
-            console.log('Pair:', pair);
-          });
-
-          console.groupEnd();
-        });
-      }
-
-      if (deletion.length) {
-        operation = operation.then(() => {
-          console.group('[SW]:', 'Deleting changed assets');
-          return deletion.map((req) => {
-            console.log('Asset:', req._urlString);
-            return cache.delete(req);
-          });
-          console.groupEnd();
-        }).then((deletion) => {
-          return Promise.all(deletion);
-        });
-      }
-
-      return operation;
-    }).then(() => {
-      return caches.open(CACHE_TMP);
-    }).then((tmpCache) => {
-      return Promise.all([
-        tmpCache,
-        tmpCache.keys(),
-        tmpCache.matchAll(),
-      ]);
-    }).then((data) => {
-      const tmpCache = data[0];
-      const requests = data[1];
-      const responses = data[2];
-
-      if (!requests.length) return;
-
-      const all = requests.map((req, i) => {
-        return cache.put(req, responses[i]);
-      });
-
-      return Promise.all(all);
-    }).then(() => {
-      return caches.delete(CACHE_TMP);
-    });
+    })
+    .catch(() => {})
   }
 
   self.addEventListener('fetch', (event) => {
@@ -327,8 +241,7 @@ function WebpackServiceWorker(params) {
     }
 
     const resource = caches.match(urlString, {
-      cacheName: CACHE_NAME,
-      ignoreSearch: true
+      cacheName: CACHE_NAME
     }).then((response) => {
       if (response) {
         if (DEBUG) {
@@ -339,11 +252,8 @@ function WebpackServiceWorker(params) {
       }
 
       // Load and cache known assets
-      return fetch(urlString).then((response) => {
-        if (
-          !response || response.status !== 200 ||
-          !(response.type === 'basic' || response.type === 'cors')
-        ) {
+      return fetch(event.request).then((response) => {
+        if (!response || !response.ok) {
           if (DEBUG) {
             console.log(
               '[SW]:',
@@ -393,6 +303,14 @@ function WebpackServiceWorker(params) {
         return url.toString();
       });
     });
+
+    hashesMap = Object.keys(hashesMap).reduce((result, hash) => {
+      const url = new URL(hashesMap[hash], location);
+      url.search = '';
+
+      result[hash] = url.toString();
+      return result;
+    }, {});
   }
 
   function applyCacheBust(asset, key) {
@@ -401,10 +319,18 @@ function WebpackServiceWorker(params) {
   }
 }
 
-function addAllNormalized(cache, requests) {
-  return Promise.all(requests.map((request) => fetch(request))).then((responses) => {
+function addAllNormalized(cache, requests, options) {
+  const bustValue = options && options.bust;
+
+  return Promise.all(requests.map((request) => {
+    if (bustValue) {
+      request = applyCacheBust(request, bustValue);
+    }
+
+    return fetch(request);
+  })).then((responses) => {
     if (responses.some(response => !response.ok)) {
-      return Promise.reject(new Error('Response not supported'));
+      return Promise.reject(new Error('Wrong response status'));
     }
 
     const addAll = responses.map((response, i) => {
