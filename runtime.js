7cbeb7ea3e172d29c32c71e7af1c233c27941ed7
Arthur Stolyar <nekr.fabula@gmail.com>
2016-06-17 09:19:43 +0300

    Add warning to runtime.js about development mode
    Fixes #74

--- runtime.js
+++ runtime.js
@@ -1,2 +1,13 @@
+var warn = "offline-plugin: runtime.install() was executed without OfflinePlugin being added to webpack.config.js. " +
+  "The installation of offline-plugin has not been performed. " +
+  "You can ignore this message if you are skipping the OfflinePlugin during development. " +
+  "Otherwise, you may have a configuration issue.";
+
+if (console.warn) {
+  console.warn(warn);
+} else if (console.log) {
+  console.log(warn);
+}
+
 exports.install = function() {};
 exports.applyUpdate = function() {};
