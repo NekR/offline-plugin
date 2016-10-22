32c4b98fd63423cfd6cf84a2d614bcf61f97ece7
Arthur Stolyar <nekr.fabula@gmail.com>
2016-10-22 14:34:25 +0300

    Update warning for runtime without a config

--- runtime.js
+++ runtime.js
@@ -1,12 +1,11 @@
-var warn = "offline-plugin: runtime.install() was executed without OfflinePlugin being added to webpack.config.js. " +
-  "The installation of offline-plugin has not been performed. " +
-  "You can ignore this message if you are skipping the OfflinePlugin during development. " +
-  "Otherwise, you may have a configuration issue.";
+var warn = "offline-plugin: runtime was installed without OfflinePlugin being added to the webpack.config.js. See https://goo.gl/2Ca7NO for details.";
 
-if (console.warn) {
-  console.warn(warn);
-} else if (console.log) {
-  console.log(warn);
+if (window.console) {
+  if (console.info) {
+    console.info(warn);
+  } else if (console.log) {
+    console.log(warn);
+  }
 }
 
 exports.install = function() {};
