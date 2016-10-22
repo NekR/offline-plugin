2ffacfb035083c62cda2699efef72bf4b275414e
Arthur Stolyar <nekr.fabula@gmail.com>
2016-10-22 16:01:24 +0300

    Ver. 4.4.0

--- tests/legacy/fixtures/basic-with-plugin-version/__expected/sw.js
+++ tests/legacy/fixtures/basic-with-plugin-version/__expected/sw.js
@@ -14,6 +14,6 @@ var __wpo = {
   "responseStrategy": "cache-first",
   "version": "3b59d46a1a71fdd7f971",
   "name": "webpack-offline",
-  "pluginVersion": "4.3.1",
+  "pluginVersion": "4.4.0",
   "relativePaths": true
 };
