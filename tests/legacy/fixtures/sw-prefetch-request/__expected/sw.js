34e4351d31de8c9020a9bd405d297bee0eaf3898
Arthur Stolyar <nekr.fabula@gmail.com>
2016-10-22 15:59:56 +0300

    Add missed tests

---
+++ tests/legacy/fixtures/sw-prefetch-request/__expected/sw.js
@@ -0,0 +1,22 @@
+var __wpo = {
+  "assets": {
+    "main": [
+      "./external.js"
+    ],
+    "additional": [],
+    "optional": []
+  },
+  "externals": [
+    "./external.js"
+  ],
+  "hashesMap": {},
+  "strategy": "changed",
+  "responseStrategy": "cache-first",
+  "version": "3b59d46a1a71fdd7f971",
+  "name": "webpack-offline",
+  "relativePaths": true,
+  "prefetchRequest": {
+    "credentials": "include",
+    "mode": "cors"
+  }
+};
