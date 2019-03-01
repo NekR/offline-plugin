94b1b5a4e37c70aaa07e86f83795db19935d096a
Alan Smithee <ggnore.alan.smithee@gmail.com>
2019-03-01 11:33:24 +0100

    add scope registration test

---
+++ tests/browser/sw-is-registered-with-scope/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const { resolve, join } = path;
+
+const OfflinePlugin = require(process.env.OFFLINE_PLUGIN_ROOT);
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+
+const webpackMajorVersion = require('webpack/package.json').version.split('.')[0];
+
+const config = {
+  entry: {
+    main: join(__dirname, 'src/main.js'),
+  },
+
+  output: {
+    path: join(__dirname, 'www'),
+    filename: '[name].js',
+    chunkFilename: '[name].js',
+    publicPath: '/'
+  },
+
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: join(__dirname, 'src/index.html') },
+      { from: join(__dirname, 'src/app-shell.html') }
+    ]),
+    new DefinePlugin({
+      'process.env.OFFLINE_PLUGIN_ROOT': JSON.stringify(process.env.OFFLINE_PLUGIN_ROOT)
+    }),
+    new OfflinePlugin({
+      ServiceWorker: {
+        scope: '/app',
+      }
+    })
+  ]
+};
+
+if (webpackMajorVersion === '4') {
+  config.mode = 'none';
+}
+
+module.exports = config;
