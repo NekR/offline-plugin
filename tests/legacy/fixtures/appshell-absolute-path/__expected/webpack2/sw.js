08e545e4e602d03ef54f60b56bffd5dd6fd10565
Sergei Buinov <sergey.buinov@emergn.com>
2019-03-07 13:29:30 +0300

    fix tests for webpack version 2 & 3

--- tests/legacy/fixtures/appshell-absolute-path/__expected/webpack2/sw.js
+++ tests/legacy/fixtures/appshell-absolute-path/__expected/webpack2/sw.js
@@ -522,9 +522,16 @@ function WebpackServiceWorker(params, helpers) {
     });
   }
 
+  function shouldServeFromNetwork(response, urlString, cacheUrl) {
+    if (params.shouldServeFromNetwork) {
+      return params.shouldServeFromNetwork(response, urlString, cacheUrl);
+    }
+    return response.ok;
+  }
+
   function networkFirstResponse(event, urlString, cacheUrl) {
     return fetchWithPreload(event).then(function (response) {
-      if (response.ok) {
+      if (shouldServeFromNetwork(response, urlString, cacheUrl)) {
         if (DEBUG) {
           console.log('[SW]:', 'URL [' + urlString + '] from network');
         }
@@ -869,6 +876,7 @@ function logGroup(title, assets) {
 
   console.groupEnd();
 }
+
         WebpackServiceWorker(__wpo, {
 loaders: {},
 cacheMaps: [
--- tests/legacy/fixtures/cachemaps-basic/__expected/webpack3/sw.js
+++ tests/legacy/fixtures/cachemaps-basic/__expected/webpack3/sw.js
@@ -509,9 +509,16 @@ function WebpackServiceWorker(params, helpers) {
     });
   }
 
+  function shouldServeFromNetwork(response, urlString, cacheUrl) {
+    if (params.shouldServeFromNetwork) {
+      return params.shouldServeFromNetwork(response, urlString, cacheUrl);
+    }
+    return response.ok;
+  }
+
   function networkFirstResponse(event, urlString, cacheUrl) {
     return fetchWithPreload(event).then(function (response) {
-      if (response.ok) {
+      if (shouldServeFromNetwork(response, urlString, cacheUrl)) {
         if (DEBUG) {
           console.log('[SW]:', 'URL [' + urlString + '] from network');
         }
@@ -856,6 +863,7 @@ function logGroup(title, assets) {
 
   console.groupEnd();
 }
+
         WebpackServiceWorker(__wpo, {
 loaders: {},
 cacheMaps: [
